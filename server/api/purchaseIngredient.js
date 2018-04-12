ea0e1ed0df44c9dc392f034b87f7744d30167f1a
Rian Halperin <rianhalperin@gmail.com>
2018-04-11 23:02:10 -0400

    feat(purchaseIngredient): create backend get all route

--- server/api/purchaseIngredient.js
+++ server/api/purchaseIngredient.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const { PurchaseIngredient } = require('../db/models');
 
+router.get('/', (req, res, next) => {
+  PurchaseIngredient.findAll()
+    .then(ingredients => res.json(ingredients))
+    .catch(next)
+})
+
 router.post('/', function (req, res, next) {
   PurchaseIngredient.create(req.body)
   .then(ingredient => res.json(ingredient))
