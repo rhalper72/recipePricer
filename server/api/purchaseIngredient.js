c403811e3223ef7a3a9d32786e266698f53181a9
Rian Halperin <rianhalperin@gmail.com>
2018-04-11 23:19:52 -0400

    feat(purchaseIngredient): create backend get one route for ingredientId

--- server/api/purchaseIngredient.js
+++ server/api/purchaseIngredient.js
@@ -3,13 +3,21 @@ const { PurchaseIngredient } = require('../db/models');
 
 router.get('/', (req, res, next) => {
   PurchaseIngredient.findAll()
-    .then(ingredients => res.json(ingredients))
+    .then(purchIngredients => res.json(purchIngredients))
     .catch(next)
+})
+
+router.get('/ingredient/:ingredientId', (req, res, next) => {
+  PurchaseIngredient.findOne({
+      where: {ingredientId: req.params.ingredientId}
+    })
+  .then(purchIngredient => res.json(purchIngredient))
+  .catch(next)
 })
 
 router.post('/', function (req, res, next) {
   PurchaseIngredient.create(req.body)
-  .then(ingredient => res.json(ingredient))
+  .then(purchIngredient => res.json(purchIngredient))
   .catch(next)
 })
 
